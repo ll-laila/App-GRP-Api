08ea324eff1b3f10d761bca9aa9f932df924f290
ll-laila <lailatimasli1@gmail.com>
2024-08-14 18:38:38 +0100

    deploy nodejs to ec2

--- index.js
+++ index.js
@@ -3,28 +3,10 @@ const stripe = require('stripe')('sk_test_51P4gaDP3zlq0Z21j88E1KCzm1r9jrJdyudZUj
 
 const cors = require('cors');
 
-import bodyParser from 'body-parser';
-
-
-const port = 4000;
 const app = express();
 
-app.use((_req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-
-    next();
-});
-
-// Parses the text as url encoded data
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Parses the text as json
-app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello World, from express');
-})
+app.use(cors());
+app.use(express.json());
 
 app.post('/stripe_checkout', async (req, res) => {
     const stripeToken = req.body.stripeToken;
@@ -48,7 +30,14 @@ app.post('/stripe_checkout', async (req, res) => {
         res.status(500).json({ error: 'Payment failed, and refund issued.' });
     }
 });
-//
-app.listen(port, function () {
-    console.log("Server is listening at port:" + port);
+
+//home
+app.get('/', (req, res) => {
+    res.send('Hello World, from express');
+})
+
+app.listen(4000, () => {
+    console.log('Server listening on port 4000');
 });
+
+
